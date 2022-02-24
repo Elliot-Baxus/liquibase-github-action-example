4b403de46d756229b736c4cb57a237c27b69c16c
Elliot Weiss <elliot@Elliots-MacBook-Air.local>
2022-02-24 08:32:21 -0700

    Adding new changeset with tag

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -8,6 +8,15 @@ create table Person (
     address1 varchar(50),
     address2 varchar(50),
     city varchar(30),
-    us_state varchar(30)
+)
+
+--changeset Elliot-BAXUS:2
+--rollback DROP TABLE Person_2;
+create table Person_2 (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30),
 )
 
