1bce5d55869be2e7c0ef810905f1ea7a086b3f85
Elliot Weiss <elliot@Elliots-MacBook-Air.local>
2022-02-24 08:01:33 -0700

    Testing with mySQL

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -10,31 +10,3 @@ create table Person (
     city varchar(30)
 )
 
---changeset Elliot-BAXUS:2
---rollback DROP TABLE company;
-create table company (
-    id int primary key,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
-
---changeset other.dev:3
---rollback ALTER TABLE person DROP COLUMN country;
-alter table person add column country varchar(2)
-
---changeset other.dev:4
---rollback ALTER TABLE person DROP COLUMN state;
-alter table person add column state varchar(2)
-
---changeset other.dev:5
---rollback ALTER TABLE company DROP COLUMN country;
-alter table company add column country varchar(2)
-
---changeset Elliot:Elliot1
---rollback DROP TABLE Elliot;
-CREATE TABLE Elliot (
-    id int primary key,
-    name varchar(50) not null
-)
